c92759748347feb4de9a481eb105265dddf8bcb8
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-03-04 12:05:03 -0800

    add controllers/routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.create(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      phone_number: params["phone_number"]
+    )
+    if @user.valid? #happy path
+      render :show
+    else #sad path
+      render json: { message: { errors: @user.errors.full_messages } },
+             status: 422
+    end
+  end
+
+  def update
+    @user = User.update(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      phone_number: params["phone_number"]
+    )
+    if @user.valid? #happy path
+      render :show
+    else #sad path
+      render json: { message: { errors: @user.errors.full_messages } },
+             status: 422
+    end
+  end
+
 end
+  
